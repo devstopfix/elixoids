65b5df06edfce044af4263e5bb9c483c821b351d
devstopfix <james@devstopfix.net>
2016-05-22 16:13:55 +0100

    Points of compass

--- lib/world/velocity.ex
+++ lib/world/velocity.ex
@@ -5,6 +5,20 @@ defmodule World.Velocity do
   2) a speed in metres/second.
   """
 
+  alias World.Velocity, as: Velocity
+
   defstruct theta: 0.0, speed: 0.0
+
+  def north do
+    %Velocity{ theta: (:math.pi / 2) }
+  end
+
+  def west do
+    %Velocity{ theta: :math.pi }
+  end
+
+  def south do
+    %Velocity{ theta: (3 * :math.pi / 2) }
+  end
 
 end
