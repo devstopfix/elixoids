521faf3899ead9e28f1abb7b0372a05c9d0cdebf
devstopfix <james@devstopfix.net>
2016-05-25 23:39:41 +0100

    Travis

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Elixoids.Mixfile do
 
   def project do
     [app: :elixoids,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
