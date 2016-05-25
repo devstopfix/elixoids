4329622e9fe0d1377f16e9d83dd22b87535cbb8c
devstopfix <james@devstopfix.net>
2016-05-25 23:38:00 +0100

    WIP Asteroid process

--- lib/game/asteroid.ex
+++ lib/game/asteroid.ex
@@ -7,15 +7,25 @@ defmodule Game.Asteroid do
    use GenServer
 
    def start_link do
-     GenServer.start_link(__MODULE__, :ok, [])
+     a = %{:pos => %World.Point{}, 
+           :velocity => %World.Velocity{},
+           :radius => 10.0}
+     GenServer.start_link(__MODULE__, a, [])
    end
 
-   def init(:ok) do
-     {:ok, %{}}
+   def init(a) do
+     {:ok, a}
+   end
+
+   def move(asteroid, delta_t_ms) do
+     GenServer.cast(asteroid, {:move, delta_t_ms})
    end
 
    def handle_cast({:move, delta_t_ms}, asteroid) do
      {:noreply, asteroid}
    end
+
+   # {:ok, a} = Game.Asteroid.start_link
+   # Game.Asteroid.move(a,1)
 
 end
