6d46bb4dab624fe31fea0970a9b29600c1eb7da9
devstopfix <james@devstopfix.net>
2016-05-30 12:03:42 +0100

    Identifier generator

---
+++ test/game/identifers_test.exs
@@ -0,0 +1,23 @@
+defmodule Game.IdentifiersTest do
+  use ExUnit.Case, async: false
+  use ExCheck
+  doctest Game.Identifiers
+
+  alias Game.Identifiers, as: Identifiers
+
+  # Make an id generator, get a sequence of numbers, ensure consecutive
+  property :generate_n_identifiers do
+    for_all {n} in {pos_integer} do
+
+      {:ok, pid} = Identifiers.start_link
+
+      ids = Enum.reduce(Range.new(1, n), [], 
+        fn (_i, acc) -> [Identifiers.next(pid) | acc] end)
+
+      expected = Range.new(1, n) |> Enum.to_list |> Enum.reverse
+
+      assert ids == expected
+    end
+  end
+
+end
