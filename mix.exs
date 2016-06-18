231ac7cf1276755fb320c00127cfdee936d89284
devstopfix <james@devstopfix.net>
2016-06-18 19:08:32 +0100

    Fix perturb

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Elixoids.Mixfile do
     [app: :elixoids,
      description: "Asteroids game server",
      name: "Elixoids",
-     version: "0.5.1",
+     version: "0.5.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
