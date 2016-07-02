07b6d4d5d7d39cfe6067ecea53633d20917abdfa
James Every <james.every@zuhlke.com>
2016-07-02 21:39:52 +0100

    Can spawn and remove players by tag

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Elixoids.Mixfile do
     [app: :elixoids,
      description: "Asteroids game server",
      name: "Elixoids",
-     version: "0.7.0",
+     version: "0.7.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
