33fb05aafd5db9b5fecdf64947bee1a764ed6c26
James Every <james.every@zuhlke.com>
2016-07-04 18:53:56 +0100

    Rocks

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Elixoids.Mixfile do
     [app: :elixoids,
      description: "Asteroids game server",
      name: "Elixoids",
-     version: "0.8.4",
+     version: "0.9.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
