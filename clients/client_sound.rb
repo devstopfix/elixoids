c41da926c78ad2f9ae90abb2dfeba8477c674e31
devstopfix <devstopfix@gmail.com>
2019-04-10 18:39:16 +0100

    Refactor client to same library

--- clients/client_sound.rb
+++ clients/client_sound.rb
@@ -1,35 +1,36 @@
-require 'websocket-client-simple'
+require 'faye/websocket'
+require 'eventmachine'
 require 'json'
 
 $SERVER = ENV['ELIXOIDS_SERVER'] || 'localhost:8065'
-$SOCKET = "ws://#{$SERVER}/sound"
 
 $finished = false
 
-WebSocket::Client::Simple.connect $SOCKET do |ws|
+def run()
 
-  ws.on :open do
-    puts "Connected to " << $SOCKET
-  end
+  url = "ws://#{$SERVER}/sound"
 
-  ws.on :message do |msg|
-    state = JSON.parse(msg.data)
-    explosions = state['x']
-    puts explosions.inspect unless explosions.empty?
-  end
+  EM.run {
+    ws = Faye::WebSocket::Client.new(url)
 
-  ws.on :error do |e|
-    p e
-    $finished = true
-  end
 
-  ws.on :close do |e|
-    p e
-    puts "GAME OVER!"
-    $finished = true
-  end
+    ws.on :message do |msg|
+      state = JSON.parse(msg.data)
+      explosions = state['x']
+      puts explosions.inspect unless explosions.empty?
+    end
 
+    ws.on :error do |e|
+      p e
+      $finished = true
+    end
+
+    ws.on :close do |e|
+      p e
+      puts "GAME OVER!"
+      $finished = true
+    end
+  }
 end
 
-while not $finished do
-end
+run()
