1a4bb5f32a8c905ff235c727a7c4993e40819cbc
devstopfix <devstopfix@gmail.com>
2019-04-11 11:18:15 +0100

    Fix and refactor explosion test

--- test/game/explosion_test.exs
+++ test/game/explosion_test.exs
@@ -1,21 +1,13 @@
 defmodule Game.ExplosionTest do
   use ExUnit.Case, async: true
-  doctest Game.Explosion
 
-  alias Game.Explosion, as: Explosion
-
-  @t_past 1_471_605_231_831
-  @t_future 2_271_605_231_831
-
-  test "Default explosion struct" do
-    ex = %Explosion{}
-
-    assert 0.0 = ex.x
-    assert 0.0 = ex.y
-  end
+  alias Elixoids.News
+  alias Game.Server, as: Game
 
   test "Convert struct to state sent to client" do
-    ex = Explosion.at_xy(1.0, 2.0)
-    assert [1.0, 2.0] == Explosion.to_state(ex)
+    {:ok, pid} = Game.start_link(2, 1)
+    News.subscribe(0)
+    Game.explosion(pid, 1.0, 2.0)
+    assert_receive {:explosion, [1.0, 2.0]}, 500
   end
 end
