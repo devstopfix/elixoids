3de81f55909f43cb520c011fe31ff2d058d9ca22
J Every <devstopfix@gmail.com>
2023-05-03 06:52:57 +0100

    Return sound events as cmds to JS

---
+++ priv/html/audio.js
@@ -0,0 +1,64 @@
+'use strict';
+
+function toggleSound() {
+    if (fxAudioContext == undefined) {
+        initFX();
+    } else {
+        fxAudioContext = undefined;
+    }
+}
+
+function initFX() {
+    try {
+        fxAudioContext = new AudioContext();
+        var root = [window.location.protocol, window.location.host].join('//');
+        for (var i = 1; i <= 7; i++) {
+            var url = [root, 'audio', 'explosion', i + '.mp3'].join('/');
+            loadSoundBuffer(url, fxAudioBuffers.explosion);
+        }
+        return true;
+    }
+    catch (e) {
+        console.error('Web Audio API is not supported in this browser');
+        console.error(e)
+        return false;
+    }
+}
+
+function loadSoundBuffer(url, target) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.responseType = 'arraybuffer';
+
+    request.onload = function () {
+        fxAudioContext.decodeAudioData(request.response, function (buffer) {
+            target.push(buffer);
+        }, function (e) {
+            console.error(e);
+        });
+    }
+    request.send();
+}
+
+function testSound() {
+    var source = fxAudioContext.createBufferSource();
+    source.buffer = fxAudioBuffers.explosion[0];
+    var gainFadeOut = fxAudioContext.createGain();
+    gainFadeOut.gain.setValueAtTime(gainFadeOut.gain.value - 0.2, fxAudioContext.currentTime);
+    source.connect(gainFadeOut)
+    gainFadeOut.connect(fxAudioContext.destination);       // connect the source to the context's destination (the speakers)
+    gainFadeOut.gain.exponentialRampToValueAtTime(0.01, fxAudioContext.currentTime + 4);
+    source.start();
+}
+
+function playExplosion(audio) {
+    var index = (audio.index || 0) % fxAudioBuffers.explosion.length;
+    var source = fxAudioContext.createBufferSource();
+    source.buffer = fxAudioBuffers.explosion[index];
+    var gainFadeOut = fxAudioContext.createGain();
+    gainFadeOut.gain.setValueAtTime(gainFadeOut.gain.value - 0.2, fxAudioContext.currentTime);
+    source.connect(gainFadeOut)
+    gainFadeOut.connect(fxAudioContext.destination);
+    gainFadeOut.gain.exponentialRampToValueAtTime(0.01, fxAudioContext.currentTime + 4);
+    source.start();
+}
