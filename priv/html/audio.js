7f0d26df18708dcd64e172df28e623fb9feec6a8
J Every <devstopfix@gmail.com>
2023-05-04 21:55:14 +0100

    Check empty audio buffer

--- priv/html/audio.js
+++ priv/html/audio.js
@@ -14,6 +14,7 @@ function initFX() {
         fxAudioContext = new AudioContext();
         document.getElementById('toggle_audio').innerText = "AUDIO 🔉";
         var root = [window.location.protocol, window.location.host].join('//');
+        // TODO check if we have empty buffer before reloading (or length is too short)
         for (var i = 1; i <= 7; i++) {
             var url = [root, 'audio', 'explosion', i + '.mp3'].join('/');
             loadSoundBuffer(url, fxAudioBuffers.explosion);
@@ -43,15 +44,16 @@ function loadSoundBuffer(url, target) {
     request.send();
 }
 
-function playExplosion(audio) {
-    // TODO check for empty buffer list
-    var index = (audio.index || 0) % fxAudioBuffers.explosion.length;
-    var source = fxAudioContext.createBufferSource();
-    source.buffer = fxAudioBuffers.explosion[index];
-    var gainFadeOut = fxAudioContext.createGain();
-    gainFadeOut.gain.setValueAtTime(gainFadeOut.gain.value - 0.2, fxAudioContext.currentTime);
-    source.connect(gainFadeOut)
-    gainFadeOut.connect(fxAudioContext.destination);
-    gainFadeOut.gain.exponentialRampToValueAtTime(0.01, fxAudioContext.currentTime + 2);
-    source.start();
+function playExplosion(audio, buffers) {
+    if (buffers.length > 0) {
+        var index = (audio.index || 0) % buffers.length;
+        var source = fxAudioContext.createBufferSource();
+        source.buffer = buffers[index];
+        var gainFadeOut = fxAudioContext.createGain();
+        gainFadeOut.gain.setValueAtTime(gainFadeOut.gain.value - 0.2, fxAudioContext.currentTime);
+        source.connect(gainFadeOut)
+        gainFadeOut.connect(fxAudioContext.destination);
+        gainFadeOut.gain.exponentialRampToValueAtTime(0.01, fxAudioContext.currentTime + 2);
+        source.start();
+    }
 }
